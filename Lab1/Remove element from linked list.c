e2d5bbd3941710ad1f085836c6ecbef6634fa001
Chekwube Ononuju <chekwube.s.ononuju@gmail.com>
2025-02-21 16:57:40 -0600

    Add files via upload

---
+++ Lab1/Remove element from linked list.c
@@ -0,0 +1,25 @@
+
+
+/* Function to remove elements from a linked list */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    // Create a dummy node that points to head
+    struct ListNode dummy;
+    dummy.next = head;
+    
+    // Use a pointer to traverse the list
+    struct ListNode *current = &dummy;
+    
+    while (current->next != NULL) {
+        if (current->next->val == val) {
+            // Remove the node
+            struct ListNode *temp = current->next;
+            current->next = current->next->next;
+            free(temp);
+        } else {
+            current = current->next;
+        }
+    }
+    
+    return dummy.next;  // Return new head (may be updated)
+}
+
